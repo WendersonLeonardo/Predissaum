817208864c8c53602e0ff6d16068bbaa4c2813a0
WendersonLeonardo <wenderson359@gmail.com>
2023-01-14 21:11:45 -0300

    mudanças

--- app_predissaum_streamlit.py
+++ app_predissaum_streamlit.py
@@ -15,43 +15,25 @@ st.set_page_config(
     }
 )
 
-dados = pd.read_csv('logsDiarios.csv')
-#dados
+dadosA = pd.read_csv('logsDiarios.csv')
+dadosB = pd.read_csv('dados pred.csv')
+df = pd.read_csv('erros predissaum.csv')
 
-max = len(dados['value'])
+maxA = len(dadosA['index'])
+maxB = len(dadosB['index'])
+altura = 400
 
 #d = dados.where(dados['index'] < 5) ---> gera NaN
-d1 = dados.loc[(dados['index'] < 5)]
-d1
 
-with st.sidebar:
-    st.subheader('menu de opções para alternar entre as possibilidades das combinações de redes')
-    col1, col2 = st.columns(2)
-
-    with col1:
-        camada1 = st.selectbox(
-            'Primeira Camada',
-            options=['LSTM','GRU'])
-
-    with col2:
-        camada2 = st.selectbox(
-            'Segunda Camada',
-            options=['LSTM','GRU'])
-    
-    qtd_neuro = st.selectbox(
-            'Quantidade de Neuronios',
-            options=['camada1 = 56 | camada2 = 32','camada1 = 81 | camada2 = 27'])
-    
-    st.subheader('seletor linha (2 pontos)')
-    inter_min, inter_max = st.slider('escolha a qtd de unidades', 1,max,(1,max))
-    st.write('intervalo escolhido: ', inter_min, inter_max)
 
 
 st.header('Base')
-st.subheader('Selecione, caso deseje, um intervalo para a base abaixo')
-inter_min1, inter_max1 = st.slider(1,max,(1,max))
+def converter(df):
+    return df.to_csv().encode('utf-8')
 
-linhaA = alt.Chart(dados).mark_line(
+inter_minA, inter_maxA = st.slider('Selecione, caso deseje, um intervalo para a base abaixo', 1,maxA,(1,maxA))
+
+linhaA = alt.Chart(dadosA.loc[(dadosA['index'] >= inter_minA)&(dadosA['index'] <= inter_maxA)]).mark_line(
     color='lightgreen'
 ).encode(
     x = alt.X('index'),
@@ -59,13 +41,82 @@ linhaA = alt.Chart(dados).mark_line(
     tooltip=['index','value']
 ).properties(
     width=600,
-    height=300,
+    height=altura,
     title = 'valores mensais'
 )
 st.altair_chart(linhaA,use_container_width=True)
 
-st.subheader('combinassaum do grafico da base vs o predito')
+st.download_button(
+    label='baixar base.csv',
+    data=converter(dadosA),
+    file_name='base predissaum.csv',
+    mime='text/csv  '
+)
 
-st.subheader('download da imagem')
+st.header('Root Mean Squared Error')
+st.write('Erro Quadrático Médio')
+barras = alt.Chart(df).mark_bar(width=10
+).encode(x ='combinassaum', y ='error',
+    color='combinassaum').properties(
+    height=altura-20,
+    width=700,
+    title = 'taxa de erro'
+)
 
-st.subheader('grafico comparando a taxa de acerto')
+rotuloB = barras.mark_text(
+    dy = -11,
+    size = 15
+).encode(
+    text='error')
+
+st.altair_chart(barras+rotuloB)
+
+
+
+#st.header()
+
+st.subheader('Menu de opções para alternar entre as possibilidades das combinações de redes')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    camada1 = st.selectbox(
+        'Primeira Camada',
+        options=['LSTM','GRU'])
+
+with col2:
+    camada2 = st.selectbox(
+        'Segunda Camada',
+        options=['LSTM','GRU'])
+qtd_neuro = st.selectbox(
+    'Quantidade de Neuronios',
+    options=['camada1 = 56 | camada2 = 32','camada1 = 81 | camada2 = 27'])
+
+inter_minB, inter_maxB = st.slider('escolha o intervalo', 1,maxB,(1,maxB))
+
+var = camada1 + ' vs ' + camada2 + ' - ' + qtd_neuro
+st.subheader('Dado Original vs Predito: ' + var)
+
+
+linhaB = alt.Chart(dadosB.loc[(dadosB['index'] >= inter_minB)&(dadosB['index'] <= inter_maxB)]).mark_line(
+    color='lightgreen'
+).encode(
+    x = alt.X('index'),
+    y = alt.Y('original'),
+    tooltip=['index','original']
+).properties(
+    #width=700,
+    height=altura+50
+)
+
+linhaC = alt.Chart(dadosB.loc[(dadosB['index'] >= inter_minB)&(dadosB['index'] <= inter_maxB)]).mark_line(
+    color='lightblue'
+).encode(
+    x = alt.X('index'),
+    y = alt.Y(var)
+)
+
+erro = df.loc[(df['combinassaum'] == var)]['error'][0]
+st.info('RMSE: ' + str(erro))
+
+st.altair_chart(linhaB+linhaC,use_container_width=True)
